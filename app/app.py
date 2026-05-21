581dcc160a921273d5fe6456e95d29ee4f0692fd
seoyedogu <seoyedogu@naver.com>
2026-05-21 17:45:15 +0900

    refact

--- app/app.py
+++ app/app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-
+#N}6Pao4do%zfD8+3~Ytp2?-T'&Fo6|wD
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
